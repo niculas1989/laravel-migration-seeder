e0563312eeebe6ef1abd69e403480c88edeaa4c8
niculas1989 <nicolas.maranzano@libero.it>
2022-03-18 16:38:46 +0100

    added new config file - a new array

---
+++ config/trains.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    [
+        'azienda' => 'Italo',
+        'stazione_di_partenza' => 'Palermo',
+        'stazione_di_arrivo' => 'Catania',
+        'orario_di_partenza' => '08:00',
+        'orario_di_arrivo' => '12:00',
+        'codice_treno' => '201234',
+        'carrozze' => '104',
+        'in_orario' => true,
+        'cancellato' => false
+    ],
+    [
+        'azienda' => 'FrecciaRossa',
+        'stazione_di_partenza' => 'Napoli',
+        'stazione_di_arrivo' => 'Palermo',
+        'orario_di_partenza' => '09:00',
+        'orario_di_arrivo' => '17:00',
+        'codice_treno' => '204239',
+        'carrozze' => '2100',
+        'in_orario' => true,
+        'cancellato' => false
+    ],
+    [
+        'azienda' => 'Trenitalia',
+        'stazione_di_partenza' => 'Roma',
+        'stazione_di_arrivo' => 'Venezia',
+        'orario_di_partenza' => '17:00',
+        'orario_di_arrivo' => '24:00',
+        'codice_treno' => '202220',
+        'carrozze' => '2180',
+        'in_orario' => true,
+        'cancellato' => false
+    ]
+];
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,13 +5,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>My Trains</title>
 </head>
 
 <body>
-    @foreach ($trains as $train)
-    <div>{{ $train['azienda'] }}</div>
-    @endforeach
+
 </body>
 
 </html>
