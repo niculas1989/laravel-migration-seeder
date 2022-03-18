ac674546978cc691eeec877487b657df9058c5c9
niculas1989 <nicolas.maranzano@libero.it>
2022-03-18 17:22:39 +0100

    added some changes in HTMl

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,29 @@
 </head>
 
 <body>
-
+    <div class="container">
+        <div class="row">
+            @forelse ($trains as $train)
+            <div class="col-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">{{$train['azienda']}}</h5>
+                        <p class="card-subtitle mb-2 text-muted">{{$train['stazione_di_partenza']}}</p>
+                        <p class="card-subtitle mb-2 text-muted">{{$train['stazione_di_arrivo']}}</p>
+                        <p class="card-subtitle mb-2 text-muted">{{$train['orario_di_partenza']}}</p>
+                        <p class="card-subtitle mb-2 text-muted">{{$train['orario_di_arrivo']}}</p>
+                        <p class="card-subtitle mb-2 text-muted">{{$train['codice_treno']}}</p>
+                        <p class="card-subtitle mb-2 text-muted">{{$train['carrozze']}}</p>
+                    </div>
+                </div>
+            </div>
+            @empty
+            <div class="text-center">
+                <h1>NON CI SONO TRENI</h1>
+            </div>
+            @endforelse
+        </div>
+    </div>
 </body>
 
 </html>
