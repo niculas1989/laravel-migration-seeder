cfc03c3a90df29d33d534ba53f51fa41a089862c
niculas1989 <nicolas.maranzano@libero.it>
2022-03-18 15:12:29 +0100

    added info on create_trains

--- database/migrations/2022_03_18_134416_create_trains_table.php
+++ database/migrations/2022_03_18_134416_create_trains_table.php
@@ -14,7 +14,18 @@ class CreateTrainsTable extends Migration
     public function up()
     {
         Schema::table('trains', function (Blueprint $table) {
-            //
+            $table->id();
+            $table->string('azienda', 12);
+            $table->string('stazione_di_partenza', 12);
+            $table->string('stazione_di_arrivo', 12);
+            $table->dateTime('orario_di_partenza', 4)->unsigned();
+            $table->dateTime('orario_di_arrivo', 4)->unsigned();
+            $table->smallInteger('codice_treno', 4)->unsigned();
+            $table->smallInteger('carrozze', 4)->unsigned();
+            $table->boolean('in_orario', 0);
+            $table->boolean('cancellato', 0);
+
+            $table->timestamps();
         });
     }
 
